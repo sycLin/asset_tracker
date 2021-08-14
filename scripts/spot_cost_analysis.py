517ce25ad52f42cc21867e0bcb23d22a9fe95eb2
syclin <stevendazhi@gmail.com>
2021-08-15 04:31:17 +0800

    Support multi assets.

--- scripts/spot_cost_analysis.py
+++ scripts/spot_cost_analysis.py
@@ -2,7 +2,7 @@
 import argparse
 from dataclasses import dataclass
 import decimal
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 import requests
 
@@ -12,6 +12,7 @@ _DECIMAL_ZERO = decimal.Decimal('0')
 
 @dataclass
 class SpotStats:
+    """Data class to store cost and earns of a target SPOT asset."""
 
     asset_name: str
 
@@ -30,52 +31,30 @@ class SpotStats:
         return self.received / self.sold if self.sold else _DECIMAL_ZERO
 
 
-def read_trade_lines(file_path: str, asset_name: str) -> List[str]:
-    """Reads the CSV file from FTX and filters by asset name.
-
-    Only SPOT trades with USD or USDT as quote asset will be returned.
-
-    Args:
-        file_path: The path to the CSV file downloaded from FTX.
-        asset_name: Target asset, e.g., "ETH".
-
-    Returns:
-        The lines (trades) of the target asset.
-    """
-    # The FTX symbol string in the CSV file.
-    # E.g., "MAPS/USD", "ETH/USDT", etc.
-    target_symbols = (f'"{asset_name}/USD"', f'"{asset_name}/USDT"')
+def get_multiple_spot_stats(file_path: str,
+                            asset_names: List[str]) -> Dict[str, SpotStats]:
+    ret = {name: SpotStats(name) for name in asset_names}
     with open(file_path, 'r') as f:
-        lines = [
-            line for line in f
-            if line.split(',')[2] in target_symbols]
-    return lines
+        for line in f:
+            tokens = line.strip().split(',')
 
-
-def get_spot_stats(asset_name: str, trade_lines: List[str]) -> SpotStats:
-    """Analyzes the trades and gets the stats.
-
-    Args:
-        trade_lines: Those lines from CSV file, each is a trade record.
-
-    Returns:
-        The SpotStats instance.
-    """
-    ret = SpotStats(asset_name)
-
-    for line in trade_lines:
-        tokens = line.strip().split(',')
-        side = tokens[3]
-        size = decimal.Decimal(tokens[5].strip('"'))
-        price = decimal.Decimal(tokens[6].strip('"'))
-        if side == '"buy"':
-            ret.bought += size
-            ret.spent += (size * price)
-        elif side == '"sell"':
-            ret.sold += size
-            ret.received += (size * price)
-        else:
-            raise ValueError(f'Did not recognize side value: {side}')
+            # tokens[2] should be like "ETH/USD", "ETH/USDT", etc.
+            if '/' not in tokens[2]:
+                continue
+            base, quote = tokens[2].strip('"').split('/', 1)
+            if base not in asset_names or quote not in ('USD', 'USDT'):
+                continue
+            side = tokens[3]
+            size = decimal.Decimal(tokens[5].strip('"'))
+            price = decimal.Decimal(tokens[6].strip('"'))
+            if side == '"buy"':
+                ret[base].bought += size
+                ret[base].spent += (size * price)
+            elif side == '"sell"':
+                ret[base].sold += size
+                ret[base].received += (size * price)
+            else:
+                raise ValueError(f'Did not recognize side value: {side}')
     return ret
 
 
@@ -91,7 +70,8 @@ def get_current_price_from_ftx(asset_name):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a',
-                        '--asset',
+                        '--assets',
+                        nargs='+',
                         help='The target asset to analyze.',
                         required=True)
     parser.add_argument('-f',
@@ -106,24 +86,25 @@ def main():
 
     decimal.getcontext().prec = 8
 
-    trade_lines = read_trade_lines(args.file, args.asset)
-    print(f'Found {len(trade_lines)} trades.')
+    # Read trades and filter and populate SpotStats instances.
+    asset_name_to_stats = get_multiple_spot_stats(args.file, args.assets)
 
-    stats = get_spot_stats(args.asset, trade_lines)
-    print(f'Spent {stats.spent}U for {stats.bought} {args.asset}. '
-          f'({stats.get_average_buy_price()}U each.)')
-    print(f'Sold {stats.sold} {args.asset} for {stats.received}U. '
-          f'({stats.get_average_sell_price()}U each.)')
-
-    if args.include_live_price:
-        current_price = get_current_price_from_ftx(args.asset)
-        print(f'Current price on FTX is: {current_price}')
-        # calculate the +/-
-        if stats.bought > stats.sold:
-            amount_left = stats.bought - stats.sold
-            total_value = amount_left * current_price + stats.received
-            prefix = '+' if total_value > stats.spent else '-'
-            print(f'({prefix}{abs(total_value - stats.spent)})')
+    for asset_name, stats in asset_name_to_stats.items():
+        print(f'===== {asset_name} ===== ')
+        print(f'Spent {stats.spent}U for {stats.bought} {asset_name}. '
+              f'({stats.get_average_buy_price()}U each.)')
+        print(f'Sold {stats.sold} {asset_name} for {stats.received}U. '
+              f'({stats.get_average_sell_price()}U each.)')
+        if args.include_live_price:
+            current_price = get_current_price_from_ftx(asset_name)
+            print(f'Current price on FTX is: {current_price}')
+            # calculate the +/-
+            if stats.bought > stats.sold:
+                amount_left = stats.bought - stats.sold
+                total_value = amount_left * current_price + stats.received
+                prefix = '+' if total_value > stats.spent else '-'
+                print(f'({prefix}{abs(total_value - stats.spent)})')
+        print()
 
 
 if __name__ == '__main__':
